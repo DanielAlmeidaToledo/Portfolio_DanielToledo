868d01bf7a9f2b3189f2c0ddca5e7c0113671828
Daniel Toledo <dan992965@gmail.com>
2023-01-11 15:42:34 -0300

    Atualização de tecnologias

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,40 +1,70 @@
-import {
-  DiReact,
-  DiJsBadge,
-  DiHtml5,
-  DiCss3,
-  DiSass,
-  DiAngularSimple,
-} from 'react-icons/di'
+import { DiJsBadge, DiSass } from "react-icons/di";
 
-import '../styles/components/technologiescontainer.sass'
+import {
+  SiTailwindcss,
+  SiTypescript,
+  SiAngular,
+  SiReact,
+} from "react-icons/si";
+
+import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
-  { id: "react", name: "React.JS", icon: <DiReact />, text: "Components, Hooks, Router, Listas, API's." },
-  { id: "angular", name: "Angular", icon: <DiAngularSimple />, text: "Components, Renderização, Interface, Diretivas, Listas."  },
-  { id: "js", name: "JavaScript", icon: <DiJsBadge />, text: "DOM, Functions, Objects, API's."  },
-  { id: "html", name: "HTML5", icon: <DiHtml5 />, text: "HTML Semântico, Formulários, Validações."  },
-  { id: "sass", name: "Sass", icon: <DiSass />, text: "Variables, Mixins, Includes."  },
-  { id: "css", name: "CSS3", icon: <DiCss3 />, text: "Animações, Responsividade, Media Queries."  },
+  {
+    id: "react",
+    name: "React.JS",
+    icon: <SiReact />,
+    text: "Components, Hooks, Reduce, Context, Router, Listas, API's.",
+  },
+  {
+    id: "angular",
+    name: "Angular",
+    icon: <SiAngular color="#C4002B" />,
+    text: "Components, Renderização, Interface, Diretivas, Listas.",
+  },
+  {
+    id: "js",
+    name: "JavaScript",
+    icon: <DiJsBadge />,
+    text: "DOM, Functions, Objects, API's.",
+  },
+  {
+    id: "typescript",
+    name: "Typescript",
+    icon: <SiTypescript color="#3178C6" />,
+    text: "Interface, Type, Props.",
+  },
+  {
+    id: "tailwind",
+    name: "Tailwind CSS",
+    icon: <SiTailwindcss color="#38BDF8" />,
+    text: "Animações, Responsividade, Media Queries.",
+  },
+  {
+    id: "sass",
+    name: "Sass",
+    icon: <DiSass />,
+    text: "Variables, Mixins, Includes.",
+  },
 ];
 
 const TechnologiesContainer = () => {
-  return <section className="technologies-container">
-    <h2>Tecnologias</h2>
-    <div className="technologies-grid">
-      {technologies.map((tech) => (
-        <div className="technology-card" id={tech.id} key={tech.id}>
-          {tech.icon}
-          <div className="technology-info">
-            <h3>{tech.name}</h3>
-            <p>
-              {tech.text}
-            </p>
+  return (
+    <section className="technologies-container">
+      <h2>Tecnologias</h2>
+      <div className="technologies-grid">
+        {technologies.map((tech) => (
+          <div className="technology-card" id={tech.id} key={tech.id}>
+            {tech.icon}
+            <div className="technology-info">
+              <h3>{tech.name}</h3>
+              <p>{tech.text}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  </section>
-}
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default TechnologiesContainer
+export default TechnologiesContainer;

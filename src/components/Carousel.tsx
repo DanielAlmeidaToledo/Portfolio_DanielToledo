8e98b339daed5de257b077b8eea119da4787392e
Daniel Toledo <dan992965@gmail.com>
2023-03-01 18:25:41 -0300

    Stylizing projects

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -18,60 +18,88 @@ const projects = [
   {
     title: "Vivace Store",
     image: project01,
+    github: "https://github.com/DanielAlmeidaToledo/Vivace",
   },
   {
     title: "Imobiliária BZ",
     image: project02,
+    github: "https://github.com/DanielAlmeidaToledo/BZ_Imobiliaria",
   },
   {
     title: "Habits",
     image: project03,
+    github: "https://github.com/DanielAlmeidaToledo/NLW_Setup",
   },
   {
     title: "Formulário Multi-Etapas",
     image: project04,
+    github: "https://github.com/DanielAlmeidaToledo/MultiForms",
   },
   {
     title: "Toledo Sports",
     image: project05,
+    github: "https://github.com/DanielAlmeidaToledo/eCommerce_React",
   },
   {
     title: "Encontre seu Duo",
     image: project06,
+    github: "https://github.com/DanielAlmeidaToledo/NLW_eSports",
   },
   {
     title: "Dev Weather",
     image: project07,
+    github: "https://github.com/DanielAlmeidaToledo/DevWeather",
   },
   {
     title: "Dashboard Angular",
     image: project08,
+    github: "https://github.com/DanielAlmeidaToledo/DashboardAngular",
   },
 ];
 
 const Carousel = () => {
   const settings = {
-    className: "center",
-    centerMode: true,
+    customPaging: function (i: number) {
+      return (
+        <a>
+          <img src={`${baseUrl}/project0${i + 1}.png`} />
+        </a>
+      );
+    },
+    dots: true,
+    dotsClass: "slick-dots slick-thumb",
     infinite: true,
-    centerPadding: "60px",
-    slidesToShow: 3,
     speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
   };
 
   return (
-    <section className="carousel-container">
-      {/* <Slider {...settings}>
-        {projects.map((project) => {
-          return (
-            <div className="carousel-item" key={project.title}>
-              <img src={project.image} alt={project.title} />
-              <h3>{project.title}</h3>
+    <div className="carousel-container">
+      {/* <Slider {...settings}> */}
+      {projects.map((project) => {
+        const { title, image, github } = project;
+        return (
+          <div
+            className="carousel-item"
+            key={title}
+            style={{
+              background: `url(${image})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="info-item">
+              <h3>{title}</h3>
+              <a href={github} className="btn btnProject" target={"_blank"}>
+                Acessar
+              </a>
             </div>
-          );
-        })}
-      </Slider> */}
-    </section>
+          </div>
+        );
+      })}
+      {/* </Slider> */}
+    </div>
   );
 };
 

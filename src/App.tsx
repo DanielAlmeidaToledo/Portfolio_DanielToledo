8167136fe44b5a86fb15d6e94ba3ea0bea1d1c33
Daniel Toledo <dan992965@gmail.com>
2023-03-02 15:27:45 -0300

    Stylizing main Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
+import HomeContainer from "./components/HomeContainer";
 import MainContent from "./components/MainContent";
 import Sidebar from "./components/Sidebar";
-
-import video from "./assets/grad.mp4";
 
 import "./styles/components/app.sass";
 
 function App() {
   return (
     <div id="portfolio">
-      <div className="mainDiv">
-        <h1>Daniel Toledo</h1>
-        <span>Front-end Developer</span>
-      </div>
+      <HomeContainer />
       <Sidebar />
       <MainContent />
     </div>

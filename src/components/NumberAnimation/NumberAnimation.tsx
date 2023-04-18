eb1f8b51e1a4a59201d494d46e3d3098d9baa2c2
Daniel Toledo <dan992965@gmail.com>
2023-04-18 10:18:30 -0300

    refactor: ajusta number animation

--- src/components/NumberAnimation/NumberAnimation.tsx
+++ src/components/NumberAnimation/NumberAnimation.tsx
@@ -1,4 +1,5 @@
 import { useSpring, animated } from "@react-spring/web";
+import { useInView } from "react-intersection-observer";
 
 import "./numberanimation.sass";
 
@@ -10,12 +11,22 @@ const NumberAnimation = ({ value }: PropsNumber) => {
   const { number } = useSpring({
     from: { number: 0 },
     number: value,
-    delay: 1000,
+    reset: true,
     config: { mass: 1, tension: 20, friction: 10 },
   });
 
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
+
+  const props = useSpring({
+    to: {
+      opacity: inView ? 1 : 0,
+    },
+  });
+
   return (
-    <animated.div className="number">
+    <animated.div className="number" ref={ref} style={props}>
       {number.to((value) => value.toFixed(0))}
     </animated.div>
   );
